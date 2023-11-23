f07d80c4c96053316b7e73a36db1fc48bbcc3723
IamRoman-rb <roman.borla2004@gmail.com>
2023-11-23 17:07:41 -0300

    Cambios

--- src/main/java/Vista/CargarCliente.java
+++ src/main/java/Vista/CargarCliente.java
@@ -33,6 +33,7 @@ public class CargarCliente extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         membresia = new javax.swing.JComboBox<>();
         botonCarga = new javax.swing.JButton();
+        botonVolver = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -45,6 +46,8 @@ public class CargarCliente extends javax.swing.JFrame {
         membresia.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         botonCarga.setText("Cargar");
+
+        botonVolver.setText("Volver");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -64,13 +67,15 @@ public class CargarCliente extends javax.swing.JFrame {
                             .addComponent(membresia, 0, 303, Short.MAX_VALUE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(145, 145, 145)
-                        .addComponent(botonCarga)))
+                        .addComponent(botonCarga))
+                    .addComponent(botonVolver))
                 .addContainerGap(50, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(24, 24, 24)
+                .addComponent(botonVolver)
+                .addGap(1, 1, 1)
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel2)
@@ -136,6 +141,7 @@ public class CargarCliente extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botonCarga;
+    private javax.swing.JButton botonVolver;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
